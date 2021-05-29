99d22907bfbe9f7cfe272ea066b7a257fcbd2575
Jared White <m7thfleet@gmail.com>
2021-05-29 14:46:21 -0600

    Basic Doxygen support and beginnings of sample README.md

---
+++ README_TEMPLATE.md
@@ -0,0 +1,5 @@
+![ADaPt Logo](adapt-logo.png)
+# YourProject
+### [YourProject] is a [License Type]-Licensed C++[14] [application/library/thing] for [doing something awesome].
+@@REMOVE the above should be a 1-2 sentence summary of the project.
+
--- include/MathUtils/DotProduct.hpp
+++ include/MathUtils/DotProduct.hpp
@@ -1,16 +1,26 @@
-#ifndef MATHUTILS_DOTPRODUCT_HPP    //Good practice to prefix include guards with project/lib name to reduce collisions
+#ifndef MATHUTILS_DOTPRODUCT_HPP    //Good practice to prefix header guards with project/lib name to reduce collisions
 #define MATHUTILS_DOTPRODUCT_HPP
 
 //Keep includes to minimum in header files
 #include <vector>
 
-namespace MathUtils{
 
-    /*Computes the dot product of two vectors containing ints.
-     * \param a         The first vector
-     * \param b         The second vector
-     * \return          The dot product of `a` and `b`.
-     * \throws std::invalid_argument if `a` and `b` do not have the same length.
+//A description of the file for Doxygen. Needed for this file to appear in the `Files` list.
+
+//! @file DotProduct.hpp Holds declarations for DotProduct related items.
+
+
+//A description of the namespace for Doxygen. Needed for the namespace to appear in the `Namespaces` list. It only needs to exist once(you don't need to repeat it in every file)
+
+//! Holds MathUtils functions and classes.
+namespace MathUtils{
+    //Function documentation for Doxygen
+
+    /** Computes the dot product of two vectors containing ints.
+     * @param a         The first vector
+     * @param b         The second vector
+     * @return          The dot product of \p a and \p b.
+     * @throws std::invalid_argument if \p a and \p b do not have the same length.
      */
     int DotProduct(const std::vector<int>& a, const std::vector<int>& b);
 }
